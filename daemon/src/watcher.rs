2f11c3f588844afd1e00d8afcb4337030e81d03d
S0ra <s0ra@duck.com>
2026-03-02 08:23:49 -0500

    Take atomic file saves into consideration and ignore transient file emptiness.
    Most text editors save files in two steps (truncate then write),
    which is why we ignore the first empty reload.
    
    Although, this introduces an issue where intentionally emptying
    the file would not kill all active connections, since now only a
    single step happens, which is the truncation step.
    
    The user shall be instructed to delete `authorized_keys` (or save it twice)
    if he actually wants to kill all active sessions.

--- daemon/src/watcher.rs
+++ daemon/src/watcher.rs
@@ -5,8 +5,12 @@ use notify::{
 };
 use ssh_key::PublicKey;
 use std::{
+    ops::ControlFlow,
     path::Path,
-    sync::{Arc, RwLock},
+    sync::{
+        Arc, RwLock,
+        atomic::{AtomicBool, Ordering::Relaxed},
+    },
 };
 
 pub type AuthorizedKeys = Arc<RwLock<Arc<[PublicKey]>>>;
@@ -15,10 +19,13 @@ pub fn watch_authorized_keys(
     config_dir: &Path,
     sessions: Arc<SessionRegistry>,
 ) -> Res<AuthorizedKeys> {
-    let keys = Arc::new(RwLock::new(load_authorized_keys(config_dir)?.into()));
+    let authorized_keys_path = config_dir.join("authorized_keys");
+
+    let keys = Arc::new(RwLock::new(
+        load_authorized_keys(&authorized_keys_path).unwrap_or_default().into(),
+    ));
     let keys_clone = Arc::clone(&keys);
 
-    let authorized_keys_path = config_dir.join("authorized_keys");
     let mut watcher =
         recommended_watcher(move |event: notify::Result<Event>| {
             watch(event, &authorized_keys_path, &sessions, &keys_clone);
@@ -47,9 +54,16 @@ fn watch(
         EventKind::Modify(_) | EventKind::Create(_) => {
             match load_authorized_keys(authorized_keys_path) {
                 Ok(new_keys) => {
+                    if let ControlFlow::Break(()) = check_atomic_save(&new_keys)
+                    {
+                        return;
+                    }
+
                     let new_fingerprints =
                         new_keys.iter().map(fingerprint).collect();
+
                     *keys.write().unwrap() = new_keys.into();
+
                     sessions.kill_unlisted(&new_fingerprints);
                 }
                 Err(e) => print_err(&e),
@@ -69,4 +83,27 @@ fn load_authorized_keys(authorized_keys_path: &Path) -> Res<Vec<PublicKey>> {
         .iter()
         .map(|e| e.public_key().clone())
         .collect::<Vec<_>>())
+}
+
+// Most text editors save files in two steps (truncate then write),
+// which is why we ignore the first empty reload.
+//
+// Although, this introduces an issue where intentionally emptying
+// the file would not kill all active connections, since now only a
+// single step happens, which is the truncation step.
+//
+// The user shall be instructed to delete `authorized_keys` (or save it twice)
+// if he actually wants to kill all active sessions.
+
+static EMPTY_SEEN: AtomicBool = AtomicBool::new(false);
+
+fn check_atomic_save(new_keys: &[PublicKey]) -> ControlFlow<()> {
+    if new_keys.is_empty() {
+        if !EMPTY_SEEN.swap(true, Relaxed) {
+            return ControlFlow::Break(());
+        }
+    } else {
+        EMPTY_SEEN.store(false, Relaxed);
+    }
+    ControlFlow::Continue(())
 }
